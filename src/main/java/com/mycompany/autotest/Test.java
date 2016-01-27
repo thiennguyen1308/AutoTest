e9a85772b644965dff0cc4c66d3af5badfa81801
thiennguyen1308 <faber.vietnam.thien@gmail.com>
2016-01-27 17:39:29 +0700

    1

---
+++ src/main/java/com/mycompany/autotest/Test.java
@@ -0,0 +1,25 @@
+package com.mycompany.autotest;
+
+/**
+ *
+ * @author Nguyen Duc Thien
+ */
+public class Test {
+
+    public int[] FirstArray() {
+        int[] array = new int[10];
+        for (int i = 0; i < 10; i++) {
+            array[i] = i;
+        }
+        return array;
+    }
+
+    public int[] SecondArray() {
+        int[] array = new int[10];
+        for (int i = 0; i < 10; i++) {
+            array[i] = i;
+        }
+        return array;
+    }
+
+}
