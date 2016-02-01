fb22407c071240b998513fa28774e82a24c22219
ORO-MAC032 <ORO-MAC032@172.30.4.221>
2016-02-01 17:24:36 +0700

    fail 3

--- src/test/java/com/mycompany/autotest/TestNGTest.java
+++ src/test/java/com/mycompany/autotest/TestNGTest.java
@@ -7,109 +7,213 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.Parameters;
+import org.testng.asserts.Assertion;
+import org.testng.asserts.SoftAssert;
 
 /**
+ * s
  *
  * @author Nguyen Duc Thien
  */
 public class TestNGTest {
-
-    private WebDriver driver;
+    
     private String baseUrl;
     private boolean acceptNextAlert = true;
-    private StringBuffer verificationErrors = new StringBuffer();
-
+    private final StringBuffer verificationErrors = new StringBuffer();
+    private final Assertion hardAssert = new Assertion();
+    private final SoftAssert softAssert = new SoftAssert();
+    private WebDriver driver;
+    
+    @Parameters({"browser"})
     @BeforeClass(alwaysRun = true)
-    public void setUp() throws Exception {
-        driver = new FirefoxDriver();
-        baseUrl = "http://localhost:8080";
+    public void setUp(String browser) throws Exception {
+        if (browser.equalsIgnoreCase("firefox")) {
+            System.out.println("firefox");
+            driver = new FirefoxDriver();
+            driver.manage().window().maximize();
+        } else if (browser.equalsIgnoreCase("chrome")) {
+            System.out.println("chrome");
+            System.setProperty("webdriver.chrome.driver", "/Volumes/Macintosh HD/chromedriver");
+            driver = new ChromeDriver();
+            driver.manage().window().maximize();
+        }
+        baseUrl = "http://localhost:8080/";
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     }
-
-    @Test()
-    public void test() throws Exception {
-        driver.get(baseUrl + "/faber-advertiser/");
+    
+    @Test
+    public void testAuto() throws Exception {
+        driver.get(baseUrl + "faber-advertiser/");
         driver.findElement(By.id("txt_password")).clear();
-        driver.findElement(By.id("txt_password")).sendKeys("123");
+        driver.findElement(By.id("txt_password")).sendKeys("a");
         driver.findElement(By.id("txt_email_login")).clear();
-        driver.findElement(By.id("txt_email_login")).sendKeys("ledinhtuan2012@gmail.com");
+        driver.findElement(By.id("txt_email_login")).sendKeys("thien.advertiser@gmail.com");
         driver.findElement(By.xpath("//button[@type='submit']")).click();
-        driver.findElement(By.xpath("//li[3]/a/span")).click();
-        for (int second = 0;; second++) {
-            if (second >= 60) {
-                fail("timeout");
-            }
-            try {
-                if ("CP-269662407".equals(driver.findElement(By.linkText("CP-269662407")).getText())) {
-                    break;
-                } else {
-                    try {
-                        assertEquals(driver.findElement(By.linkText("CP-269662407")).getText(), "CP-269662407");
-                    } catch (Error e) {
-                        verificationErrors.append(e.toString());
-                    }
-                }
-            } catch (Exception e) {
-            }
-            Thread.sleep(1000);
-        }
-
-        driver.findElement(By.xpath("//li[4]/a/span")).click();
-        for (int second = 0;; second++) {
-            if (second >= 60) {
-                fail("timeout");
-            }
-            try {
-                if ("CP-391139268".equals(driver.findElement(By.linkText("CP-391139268")).getText())) {
-                    break;
-                }
-            } catch (Exception e) {
-            }
-            Thread.sleep(1000);
-        }
-
+        Thread.sleep(2000);
         try {
-            assertEquals(driver.findElement(By.linkText("CP-391139268")).getText(), "CP-391139268");
+            assertEquals(driver.findElement(By.xpath("//div[2]/div/div/span")).getText(), "KẾT QUẢ CHIẾN DỊCH");
         } catch (Error e) {
             verificationErrors.append(e.toString());
         }
+        try {
+            assertEquals(driver.findElement(By.xpath("//div[3]/div/div/span")).getText(), "GIÁ");
+        } catch (Error e) {
+            verificationErrors.append(e.toString());
+        }
+        driver.findElement(By.linkText("Chiến dịch")).click();
+        Thread.sleep(2000);
+        for (int second = 0;; second++) {
+            if (second >= 10) {
+                fail("timeout");
+            }
+            try {
+                if ("CP-645390271".equals(driver.findElement(By.linkText("CP-645390271")).getText())) {
+                    break;
+                }
+            } catch (Exception e) {
+            }
+            Thread.sleep(1000);
+        }
+        
+        try {
+            assertEquals(driver.findElement(By.linkText("CP-786913367")).getText(), "CP-786913367");
+        } catch (Error e) {
+            verificationErrors.append(e.toString());
+        }
+        driver.findElement(By.linkText("Kiểm tra mua hàng")).click();
+        Thread.sleep(2000);
+        for (int second = 0;; second++) {
+            if (second >= 10) {
+                fail("timeout");
+            }
+            try {
+                if ("Không tìm thấy dòng nào phù hợp".equals(driver.findElement(By.cssSelector("td.dataTables_empty")).getText())) {
+                    break;
+                }
+            } catch (Exception e) {
+            }
+            Thread.sleep(1000);
+        }
+        
         driver.findElement(By.xpath("//li[6]/a/span")).click();
-        for (int second = 0;; second++) {
-            if (second >= 60) {
-                fail("timeout");
-            }
-            try {
-                if (isElementPresent(By.cssSelector("rect..highcharts-background"))) {
-                    break;
-                }
-            } catch (Exception e) {
-            }
-            Thread.sleep(1000);
-        }
-
-        driver.findElement(By.cssSelector("rect..highcharts-background")).click();
-        // ERROR: Caught exception [ERROR: Unsupported command [mouseMoveAt | css=rect..highcharts-background | ]]
-        for (int second = 0;; second++) {
-            if (second >= 60) {
-                fail("timeout");
-            }
-            try {
-                if ("CP-269662407".equals(driver.findElement(By.linkText("CP-269662407")).getText())) {
-                    break;
-                }
-            } catch (Exception e) {
-            }
-            Thread.sleep(1000);
-        }
-
+        Thread.sleep(2000);
+        new Select(driver.findElement(By.xpath("//*[@id=\"cbo_campaign_type\"]"))).selectByVisibleText("All");
         try {
-            assertEquals(driver.findElement(By.linkText("CP-269662407")).getText(), "CP-269662407");
+            assertEquals(driver.findElement(By.linkText("CP-435846415")).getText(), "CP-435846415");
         } catch (Error e) {
             verificationErrors.append(e.toString());
         }
+        for (int second = 0;; second++) {
+            if (second >= 10) {
+                fail("timeout");
+            }
+            try {
+                if ("CP-200605663".equals(driver.findElement(By.linkText("CP-200605663")).getText())) {
+                    break;
+                }
+            } catch (Exception e) {
+            }
+            Thread.sleep(1000);
+        }
+        
+        try {
+            assertEquals(driver.findElement(By.linkText("CP-948952808")).getText(), "CP-948952808");
+        } catch (Error e) {
+            verificationErrors.append(e.toString());
+        }
+        new Select(driver.findElement(By.xpath("//*[@id=\"cbo_campaign_type\"]"))).selectByVisibleText("CPA");
+        for (int second = 0;; second++) {
+            if (second >= 10) {
+                fail("timeout");
+            }
+            try {
+                if ("Lượt tương tác".equals(driver.findElement(By.cssSelector("tspan")).getText())) {
+                    break;
+                }
+            } catch (Exception e) {
+            }
+            Thread.sleep(1000);
+        }
+        
+        try {
+            assertEquals(driver.findElement(By.linkText("CP-645390271")).getText(), "CP-645390271");
+        } catch (Error e) {
+            verificationErrors.append(e.toString());
+        }
+        try {
+            assertEquals(driver.findElement(By.linkText("CP-786913367")).getText(), "CP-786913367");
+        } catch (Error e) {
+            verificationErrors.append(e.toString());
+        }
+        new Select(driver.findElement(By.xpath("//*[@id=\"cbo_campaign_type\"]"))).selectByVisibleText("CPI");
+        for (int second = 0;; second++) {
+            if (second >= 10) {
+                fail("timeout");
+            }
+            try {
+                if ("Lượt xem".equals(driver.findElement(By.cssSelector("tspan")).getText())) {
+                    break;
+                }
+            } catch (Exception e) {
+            }
+            Thread.sleep(1000);
+        }
+        
+        try {
+            assertEquals(driver.findElement(By.linkText("CP-200605663")).getText(), "CP-200605663");
+        } catch (Error e) {
+            verificationErrors.append(e.toString());
+        }
+        try {
+            assertEquals(driver.findElement(By.linkText("CP-948952808")).getText(), "CP-948952808");
+        } catch (Error e) {
+            verificationErrors.append(e.toString());
+        }
+        new Select(driver.findElement(By.xpath("//*[@id=\"cbo_campaign_type\"]"))).selectByVisibleText("CPC");
+        for (int second = 0;; second++) {
+            if (second >= 10) {
+                fail("timeout");
+            }
+            try {
+                if ("Lượt nhấp chuột".equals(driver.findElement(By.cssSelector("tspan")).getText())) {
+                    break;
+                }
+            } catch (Exception e) {
+            }
+            Thread.sleep(1000);
+        }
+        
+        try {
+            assertEquals(driver.findElement(By.linkText("CP-435846415")).getText(), "CP-435846415");
+        } catch (Error e) {
+            verificationErrors.append(e.toString());
+        }
+        try {
+            assertEquals(driver.findElement(By.linkText("CP-925247015")).getText(), "CP-925247015");
+        } catch (Error e) {
+            verificationErrors.append(e.toString());
+        }
+        driver.findElement(By.xpath("//li[7]/a/span")).click();
+        for (int second = 0;; second++) {
+            if (second >= 10) {
+                fail("timeout");
+            }
+            try {
+                if ("Lượt xem & Nhấp chuột".equals(driver.findElement(By.cssSelector("tspan")).getText())) {
+                    break;
+                }
+            } catch (Exception e) {
+            }
+            Thread.sleep(1000);
+        }
+        
+        driver.findElement(By.cssSelector("i.icon-login")).click();
     }
-
+    
     @AfterClass(alwaysRun = true)
     public void tearDown() throws Exception {
         driver.quit();
@@ -118,7 +222,7 @@ public class TestNGTest {
             fail(verificationErrorString);
         }
     }
-
+    
     private boolean isElementPresent(By by) {
         try {
             driver.findElement(by);
@@ -127,7 +231,7 @@ public class TestNGTest {
             return false;
         }
     }
-
+    
     private boolean isAlertPresent() {
         try {
             driver.switchTo().alert();
@@ -136,7 +240,7 @@ public class TestNGTest {
             return false;
         }
     }
-
+    
     private String closeAlertAndGetItsText() {
         try {
             Alert alert = driver.switchTo().alert();
@@ -151,5 +255,5 @@ public class TestNGTest {
             acceptNextAlert = true;
         }
     }
-
+    
 }
